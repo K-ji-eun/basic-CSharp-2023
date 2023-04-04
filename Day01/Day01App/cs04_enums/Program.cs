06a63ad106c97b5c80050349df39ee6ea8dbddf9
K-ji-eun <snstkfkawldm@naver.com>
2023-04-04 17:12:36 +0900

    4월 4일 화요일

---
+++ Day01/Day01App/cs04_enums/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs04_enums
+{
+    internal class Program
+    {
+        enum HomeTown  // 같은 형태 열거
+        {
+           SEOUL = 1,   // 값 지정하지 않으면 열거형으로 숫자 변화
+           DAEJEON = 2,
+           DAEGU = 3,
+           BUSAN = 6,
+           JEJU = 9
+        };
+
+        static void Main(string[] args)
+        {
+            HomeTown myHomeTown;
+            myHomeTown = HomeTown.BUSAN;
+            
+            if (myHomeTown == HomeTown.SEOUL) {
+                Console.WriteLine("서울 출신이시군요!");
+            }
+            else if (myHomeTown != HomeTown.DAEJEON)
+            {
+                Console.WriteLine("충청도예유~");
+            }
+            else if (myHomeTown == HomeTown.DAEGU)
+            {
+                Console.WriteLine("대구여~");
+            }
+            else if (myHomeTown == HomeTown.BUSAN)
+            {
+                Console.WriteLine("부산이라예~");
+            }
+        }
+    }
+}
---
+++ Day01/Day01App/cs09_nullcondition/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs09_nullcondition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Foo myfoo = null;
+
+            //int? bar;
+            //if (myfoo != null)
+            //{
+            //    bar = myfoo.member;
+            //}
+            //else
+            //{
+            //    bar = null;
+            //}
+            // 위의 아홉줄(주석부분)을 모두 축약시킴
+            int? bar = myfoo?.member;    //  삼항연산자 보다 줄인 것
+
+        }
+    }
+
+    class Foo
+    {
+        public int? member;
+    }
+}
