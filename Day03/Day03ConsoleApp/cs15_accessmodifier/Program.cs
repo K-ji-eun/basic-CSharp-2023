6b9b0d3c71def7bb02f1172e7a1de6004d1396f1
K-ji-eun <snstkfkawldm@naver.com>
2023-04-06 17:19:01 +0900

    4월 6일 목요일

---
+++ Day03/Day03ConsoleApp/cs15_accessmodifier/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs15_accessmodifier
+{
+    class WaterHeater   // class에 거는 접근 한정자 internal
+    {
+        protected int temp; // public을 쓰면 get, setTemp쓸 필요 X
+
+        public void SetTemp(int temp)
+        {
+            if( temp < -5 || temp > 40) 
+            {
+                Console.WriteLine("범위 이탈");
+                return;
+            }
+            this.temp = temp;
+        }
+
+        public int GetTemp() 
+        { 
+            return temp; 
+        }
+
+        internal void TrunOnHeater()
+        {
+            Console.WriteLine("보일러 켭니다 : {0}", temp);    
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WaterHeater boiler = new WaterHeater();
+            boiler.SetTemp(30);
+            Console.WriteLine(boiler.GetTemp());
+            boiler.TrunOnHeater();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # basic-CSharp-2023
 부경대 2023 객체지향프로그래밍 리포지토리
+
+## 1일차
+- C# 기본
+	- .NET framework / .NET 5.0 이후
+	- Visual Studio상 C# 구성
+	- 기본 문법
+	
+## 2일차
+- C# 기본
+	- 기본 문법(변수, 메서드, 연산자, 제어)
+	
+- Win App
+	- Winforms vs WPF 개요
+	- Winforms 기초
+
+## 3일차
+- C# 기본
+	- 클래스
+	
+- Win App
+	- WinForms 컨트롤
